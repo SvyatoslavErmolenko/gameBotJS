e53eac36298e1c496ae554f894d98f719c2fd9b1
SvyatoslavErmolenko <Barashko40@gmail.com>
2022-04-05 16:19:35 +0300

    Урок 6. Функции. Область видимости и замыкание.

--- gb.js
+++ gb.js
@@ -8,10 +8,20 @@ let setNumber = function (targetNumber) { //загадываем число
   targetNumber = parseInt(Math.random() * 100); //генерация случайного числа от 0 до 100
   if (targetNumber == 0) { //если число загаданно 0
     targetNumber = 1; //то присваиваем ему значение 1
+
   }
-  console.log(targetNumber);
+
+  //console.log(targetNumber);
+  return targetNumber;
+
+};
+
+let randomNunber = function (nFunc) {
+  let rNumber = nFunc;
+
 
   let guessNumber = function (userNum) {
+
     userNum = prompt("Угадай число от 1 до 100");
     if (userNum === null) {
       alert("Игра окончина");
@@ -20,15 +30,19 @@ let setNumber = function (targetNumber) { //загадываем число
 
     if (!isNumber(userNum)) {
       alert("Введи число!");
-    } else if (userNum > targetNumber) {
+    } else if (userNum > rNumber) {
       alert("Загаданное число меньше");
-    } else if (userNum < targetNumber) {
+    } else if (userNum < rNumber) {
       alert("Загаданное число больше");
-    } else if (userNum == targetNumber) {
+    } else if (userNum == rNumber) {
       confirm("Поздравляю, Вы угадали!!!");
       return;
     }
     guessNumber(userNum);
   };
+
   return guessNumber;
 };
+
+let r = randomNunber(setNumber(1, 100));
+r();
